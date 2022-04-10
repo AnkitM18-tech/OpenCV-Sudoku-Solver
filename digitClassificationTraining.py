ab5819fe997fa80f38a4fa95f7bbb9edee739aae
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2022-04-11 03:26:11 +0530

    Splitting data - training,testing and validation

--- digitClassificationTraining.py
+++ digitClassificationTraining.py
@@ -1,9 +1,13 @@
 import numpy as np
 import cv2
 import os
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
 
 # Paths and Variables
 path = "./myData"
+testRatio = 0.2
+valRatio = 0.2
 
 images = []
 classNo = []
@@ -32,3 +36,23 @@ classNo = np.array(classNo)
 # print(classNo.shape) #(10160,)
 
 # Splitting Data
+x_train,x_test,y_train,y_test = train_test_split(images,classNo,test_size=testRatio)
+x_train,x_validation,y_train,y_validation = train_test_split(x_train,y_train,test_size=valRatio)
+
+# print(x_train.shape)
+# print(x_test.shape)
+# print(x_validation.shape)
+
+numOfSamples = []
+for x in range(noOfClasses):
+    # print(len(np.where(y_train == x)[0]))
+    numOfSamples.append(len(np.where(y_train == x)[0]))
+print(numOfSamples)
+
+# Represent the Number of Samples
+plt.figure(figsize=(10,5))
+plt.bar(range(0,noOfClasses),numOfSamples)
+plt.title("Number Of Images for each Class")
+plt.xlabel("Class ID")
+plt.ylabel("Number Of Images")
+plt.show()
