d5dc76bec333c409646cfb9057d1fda1ac8948c2
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2022-04-11 01:24:52 +0530

    drawGrid function added

--- utils.py
+++ utils.py
@@ -124,3 +124,16 @@ def displayNumbers(img,numbers,color=(0,255,0)):
             if numbers[(y+9)+x] != 0:
                 cv2.putText(img,str(numbers[(y+9)+x]),(x*secW+int(secW/2)-10,int((y+0.8)*secH)),cv2.FONT_HERSHEY_COMPLEX_SMALL,2,color,2,cv2.LINE_AA)
     return img
+
+# Draw Grid to see the warp perspective efficiently
+def drawGrid(img):
+    secW = int(img.shape[1]/9)
+    secH = int(img.shape[0]/9)
+    for i in range(0,9):
+        pt1 = (0,secH*i)
+        pt2 = (img.shape[1],secH*i)
+        pt3 = (secW*i,0)
+        pt4 = (secW*i,img.shape[0])
+        cv2.line(img,pt1,pt2,(255,255,0),2)
+        cv2.line(img,pt3,pt4,(255,255,0),2)
+    return img
